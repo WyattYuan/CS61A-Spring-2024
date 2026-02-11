0ccedb6d3cca349a88fa0d0a5e4643a7150cf8cd
WyattYuan <Wyatt049@outlook.com>
2026-02-11 11:34:19 +0800

    problem 7 done

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -291,8 +291,29 @@ class WallAnt(Ant):
 
 # END Problem 6
 
+
 # BEGIN Problem 7
 # The HungryAnt Class
+class HungryAnt(Ant):
+    name = "Hungry"
+    implemented = True
+    food_cost = 4
+    chewing_turns = 3
+
+    def __init__(self, health=1):
+        super().__init__(health)
+        self.turns_to_chew = 0
+
+    def action(self, gamestate):
+        if self.turns_to_chew > 0:
+            self.turns_to_chew -= 1
+        else:
+            if self.place and self.place.bees:
+                bee_to_eat = random_bee(self.place.bees)
+                bee_to_eat.reduce_health(bee_to_eat.health)
+                self.turns_to_chew = self.chewing_turns
+
+
 # END Problem 7
 
 
