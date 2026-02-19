630ef316d88f95c624f2a0029cae73522a0f3f90
WyattYuan <Wyatt049@outlook.com>
2026-02-19 16:32:26 +0800

    lab09 q1

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,4 +1,12 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (if (< num1 num2)
+        -1
+        (if (= num1 num2)
+            0
+            1
+        )
+    )
+)
 
 (define (make-adder num) 'YOUR-CODE-HERE)
 
